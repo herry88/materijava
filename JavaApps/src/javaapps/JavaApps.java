e5881028d3cf20c02b6a306018fd23d4e0b203e5
herry88 <herry081288@gmail.com>
2021-04-08 15:54:15 +0700

    iniProjectJavaFundamental

---
+++ JavaApps/src/javaapps/JavaApps.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapps;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class JavaApps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        System.out.println("Hello World Java");
+//    int var1 = 50; 
+//    String var2 = "Herry Prasetyo ";
+//    double var3 = 0.2; 
+//    boolean var4 = true; 
+//    char var5 = 'A';
+//    int[] var6 = {1,2,3,4,5,6,7};
+//    
+//    System.out.println("Nama: " + var1);
+//    System.out.println("Angka : " + var2);
+//    System.out.println("Angka : " + var3);
+//    System.out.println("Value : " + var4);
+//    System.out.println("Char : " + var5);
+//    
+//        for(int i : var6){
+//            System.out.println(i + " ");
+//        }
+    
+    
+    
+    }
+    
+}
